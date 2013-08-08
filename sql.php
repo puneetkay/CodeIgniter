acfb2427f672b92080a03d7b3c256e13cc265c7b
Puneet Kalra <admin@puneetk.com>
2013-08-08 22:29:40 +0100

    Added template for SQL Model

---
+++ sql.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * SQL (All-in-One) Model - 08/08/2013
+ * https://github.com/puneetkay/CodeIgniter
+ *
+ * @author: Puneet Kalra
+ * http://www.puneetk.com
+ */
+
+/****************************
+* INSTRUCTIONS
+*****************************
+* Read SQL_README.md
+****************************/
+
+
+?>
