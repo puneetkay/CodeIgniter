2556b68715a71a3e031390b6e714866b9f34113d
puneetkay <admin@puneetk.com>
2013-11-18 11:06:39 +0000

    Bug Solved - Controller Class not found

--- show_404.php
+++ show_404.php
@@ -11,7 +11,7 @@
 /****************************
 * INSTRUCTIONS
 *****************************
-* Copy and paste the code below to CI_INSTALLATION/system/core/CodeIgniter.php file.
+* Copy and paste the code below in CI_INSTALLATION/system/core/CodeIgniter.php file.
 * This will override the current show_404() function and it will call the route 
 * given on '404_override' index in routes.php file. 
 ****************************/
@@ -32,9 +32,10 @@
 function show_404(){
     // Load Router and Core classes.
     $RTR =& load_class('Router', 'core');
-
+       
     // Get class and method for 404 from routes.php
     $x = explode('/', $RTR->routes['404_override']);
+
     $class = $x[0];
     $method = (isset($x[1]) ? $x[1] : 'index');
 
@@ -43,8 +44,12 @@ function show_404(){
     $callback_method = $RTR->fetch_method();
     // Can also log here, using callback class and method.
 
-    // Create object for callback
-    $CI = new $callback_class;
+    // Load class for 404 route
+    include_once(APPPATH.'controllers/'.$class.'.php');
+    // Create object for 404 route
+    $CI = new $class;
+
+    set_status_header(404);
     call_user_func_array(array(&$CI, $method), array_slice(array($class,$method), 2));
 } // End!
 
